c441edcb7e7ed3b187b66c11d044ef115ccc497a
Arijeet10 <arijeetsocial@gmail.com>
2024-04-10 17:46:35 +0530

    blog app

--- app/api/users/signup/route.js
+++ app/api/users/signup/route.js
@@ -3,6 +3,23 @@ import { connectMongoDB } from "@/utils/libs/mongodb";
 import {NextResponse} from "next/server";
 import bcryptjs from "bcryptjs";
 
+//exporting OPTIONS async function in routes handler 
+export async function OPTIONS(request) {
+    const allowedOrigin = request.headers.get("origin");
+    const response = new NextResponse(null, {
+      status: 200,
+      headers: {
+        "Access-Control-Allow-Origin": allowedOrigin || "*",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, PATCH, DELETE, OPTIONS",
+        "Access-Control-Allow-Headers":
+          "Content-Type, Authorization, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Date, X-Api-Version",
+        "Access-Control-Max-Age": "86400",
+      },
+    });
+  
+    return response;
+  }
+
 //api to register an account
 export async function POST(req){
     try {
